efa06ee8b94e1bd3e9f135fa668af59b128b25da
Werdnik Krisztián <werdnikkrisz@gmail.com>
2020-07-22 09:49:15 +0200

    JSON Content deserializer shared project added

---
+++ src/Shared/Extensions/HttpClient.Extensions.JSON/HttpResponseMessageExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace HomeAPP.Shared.Extensions.HttpMessageConverter.JSON
+{
+    public static class HttpResponseMessageExtensions
+    {
+        public static async Task<T> DeserializeJsonAsync<T>(this HttpResponseMessage message)
+            where T : class
+        {
+            var text = await message.Content.ReadAsStringAsync();
+
+            if (text == default)
+            {
+                return default;
+            }
+
+            return JsonConvert.DeserializeObject<T>(text);
+        }
+    }
+}
