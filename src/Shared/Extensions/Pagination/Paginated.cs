1fa7db820d46072e7fb5abe63948695260696575
Werdnik Krisztián <werdnikkrisz@gmail.com>
2020-07-22 09:51:19 +0200

    Pagination shared project added

---
+++ src/Shared/Extensions/Pagination/Paginated.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeAPP.Shared.Extensions.Pagination
+{
+    public class Paginated<TModel>
+        where TModel : class
+    {
+        [JsonConstructor]
+        public Paginated(int actualPage, int pageSize, long totalItemCount, IEnumerable<TModel> data)
+        {
+            ActualPage = actualPage;
+            PageSize = pageSize;
+            TotalItemCount = totalItemCount;
+            Data = data;
+        }
+
+        public int ActualPage { get; set; }
+        public int PageSize { get; set; }
+        public long TotalItemCount { get; set; }
+
+        public IEnumerable<TModel> Data { get; set; }
+    }
+}
