f9fe4a714b862b0a1804372bac28ba7279c8789d
Werdnik Krisztián <werdnikkrisz@gmail.com>
2020-09-04 17:50:08 +0200

    Service Extensions added to the build pipeline

--- src/WebAPPs/SPA/Blazor.WASM/Program.cs
+++ src/WebAPPs/SPA/Blazor.WASM/Program.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Logging;
 using AKSoftware.Localization.MultiLanguages;
 using System.Reflection;
 using System.Globalization;
+using Blazor.Extensions.Storage;
+using HomeAPP.WebAPPs.SPA.Blazor.WASM.Extensions;
 
 namespace HomeAPP.WebAPPs.SPA.Blazor.WASM
 {
@@ -18,11 +20,11 @@ namespace HomeAPP.WebAPPs.SPA.Blazor.WASM
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.Services.AddExternalServices()
+                .AddServices()
+                .AddHttpClient(builder.HostEnvironment.BaseAddress);
+
             builder.RootComponents.Add<App>("app");
-
-            builder.Services.AddLanguageContainer(Assembly.GetExecutingAssembly());
-
-            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
         }
