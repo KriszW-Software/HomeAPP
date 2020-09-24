fa00ba85b0e4907bd16dda5429d628060490544e
Werdnik Krisztián <werdnikkrisz@gmail.com>
2020-09-24 19:37:48 +0200

    Text over image app created

---
+++ src/WebAPPs/SPA/Blazor.WASM/Shared/Components/TextOverImageType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeAPP.WebAPPs.SPA.Blazor.WASM.Shared.Components
+{
+    public enum TextOverImageType
+    {
+        Centered,
+        Left,
+        Right
+    }
+}
