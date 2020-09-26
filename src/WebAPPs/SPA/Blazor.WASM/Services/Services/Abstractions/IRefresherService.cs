07da649d6f3d07d66bb994b956c3fe80047f757b
Werdnik Krisztián <werdnikkrisz@gmail.com>
2020-09-26 21:34:59 +0200

    Refresher service created

---
+++ src/WebAPPs/SPA/Blazor.WASM/Services/Services/Abstractions/IRefresherService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeAPP.WebAPPs.SPA.Blazor.WASM.Services.Services.Abstractions
+{
+    public interface IRefresherService
+    {
+        event Action RefreshRequested;
+        void CallRequestRefresh();
+    }
+}
---
+++ src/WebAPPs/SPA/Blazor.WASM/Services/Services/Implementations/RefresherService.cs
@@ -0,0 +1,19 @@
+﻿using HomeAPP.WebAPPs.SPA.Blazor.WASM.Services.Services.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeAPP.WebAPPs.SPA.Blazor.WASM.Services.Services.Implementations
+{
+    public class RefresherService : IRefresherService
+    {
+        public event Action RefreshRequested;
+
+        public void CallRequestRefresh()
+        {
+            if (RefreshRequested != default) RefreshRequested.Invoke();
+            else throw new NullReferenceException($"The event handler of {nameof(RefreshRequested)} event is null");
+        }
+    }
+}
